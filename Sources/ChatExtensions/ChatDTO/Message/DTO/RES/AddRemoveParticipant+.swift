ff5a55b353cee3d20eb6563d4b29ceabda46eafe
Hamed Hosseini <hamed8080@gmail.com>
2023-06-24 16:31:46 +0330

    - Add AddRemoveParticipant extension for type conversation

---
+++ Sources/ChatExtensions/ChatDTO/Message/DTO/RES/AddRemoveParticipant+.swift
@@ -0,0 +1,15 @@
+//
+// AddRemoveParticipant+.swift
+// Copyright (c) 2022 ChatExtensions
+//
+// Created by Hamed Hosseini on 12/14/22
+
+import Foundation
+import ChatCore
+import ChatDTO
+
+public extension AddRemoveParticipant {
+    var requestTypeEnum: ChatMessageVOTypes? {
+        ChatMessageVOTypes(rawValue: requestType ?? -1)
+    }
+}
