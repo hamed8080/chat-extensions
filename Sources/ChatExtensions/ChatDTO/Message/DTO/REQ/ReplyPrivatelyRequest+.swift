38ffc33a29149e7a9b39f9ef62ebdd9c995aaf1a
Hamed Hosseini <hamed8080@gmail.com>
2023-08-02 19:54:12 +0330

    - Add extension conten for ReplyPrivatelyRequest

---
+++ Sources/ChatExtensions/ChatDTO/Message/DTO/REQ/ReplyPrivatelyRequest+.swift
@@ -0,0 +1,19 @@
+//
+// ReplyPrivatelyRequest+.swift
+// Copyright (c) 2022 ChatExtensions
+//
+// Created by Hamed Hosseini on 12/14/22
+
+import ChatDTO
+import ChatCore
+import Foundation
+import ChatModels
+
+extension ReplyPrivatelyRequest: Queueable, PlainTextSendable, ReplyProtocol, MessageTypeProtocol, MetadataProtocol, SystemtMetadataProtocol, SubjectProtocol {
+}
+
+public extension ReplyPrivatelyRequest {
+    var content: String? { replyContent.jsonString }
+    var subjectId: Int { threadId }
+    var _messageType: ChatCore.MessageType { ChatCore.MessageType(rawValue: self.messageType.rawValue) ?? .unknown }
+}
