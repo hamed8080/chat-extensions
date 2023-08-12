bd628eefee7df5368a97b5b337ea15ff44fe19d6
Hamed Hosseini <hamed8080@gmail.com>
2023-08-12 10:31:07 +0330

    - Add Reaction extensions initially(BETA)

---
+++ Sources/ChatExtensions/ChatDTO/Reaction/AddReactionRequest+.swift
@@ -0,0 +1,16 @@
+//
+// AddReactionRequest+.swift
+// Copyright (c) 2022 ChatExtensions
+//
+// Created by Hamed Hosseini on 12/14/22
+
+import ChatDTO
+import ChatCore
+import Foundation
+
+extension AddReactionRequest: ChatSendable, SubjectProtocol {}
+
+public extension AddReactionRequest {
+    var subjectId: Int { conversationId }
+    var content: String? { jsonString }
+}
---
+++ Sources/ChatExtensions/ChatDTO/Reaction/RactionListRequest+.swift
@@ -0,0 +1,16 @@
+//
+// RactionListRequest+.swift
+// Copyright (c) 2022 ChatExtensions
+//
+// Created by Hamed Hosseini on 12/14/22
+
+import ChatDTO
+import ChatCore
+import Foundation
+
+extension RactionListRequest: ChatSendable, SubjectProtocol {}
+
+public extension RactionListRequest {
+    var subjectId: Int { conversationId }
+    var content: String? { messageIds.jsonString }
+}
