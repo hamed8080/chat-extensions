11aee5dcfc83ed172748cdd335bf79b7ea86e734
Hamed Hosseini <hamed8080@gmail.com>
2023-09-09 20:19:23 +0330

    - Add extension for UserReactionRequest

---
+++ Sources/ChatExtensions/ChatDTO/Reaction/UserReactionRequest+.swift
@@ -0,0 +1,16 @@
+//
+// UserReactionRequest+.swift
+// Copyright (c) 2022 ChatExtensions
+//
+// Created by Hamed Hosseini on 12/14/22
+
+import ChatDTO
+import ChatCore
+import Foundation
+
+extension UserReactionRequest: ChatSendable, SubjectProtocol {}
+
+public extension UserReactionRequest {
+    var subjectId: Int { conversationId }
+    var content: String? { jsonString }
+}
