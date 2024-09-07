270fcf03a24679de87a2700d6e7a5f2c81c2e806
Hamed Hosseini <hamed8080@gmail.com>
2024-09-07 13:48:24 +0330

    Bump up the chat-transciver version to 2.2.1

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let local: [Package.Dependency] = [
 ]
 
 let remote: [Package.Dependency] = [
-    .package(url: "https://pubgi.sandpod.ir/chat/ios/chat-transceiver", from: "2.2.0"),
+    .package(url: "https://pubgi.sandpod.ir/chat/ios/chat-transceiver", from: "2.2.1"),
     .package(url: "https://pubgi.sandpod.ir/chat/ios/chat-cache", from: "2.2.0"),
     .package(url: "https://pubgi.sandpod.ir/chat/ios/chat-core", from: "2.2.0"),
     .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0"),
